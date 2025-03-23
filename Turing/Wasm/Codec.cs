fc00310ae1eeebff29e353144caee2c7bbfeb744
westbot <82739021+westbot657@users.noreply.github.com>
2025-03-23 00:30:10 -0600

    more interop setup

--- Turing/Wasm/Codec.cs
+++ Turing/Wasm/Codec.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
+using Turing.Wasm.Wrappers;
 
 namespace Turing.Wasm
 {
@@ -15,6 +17,7 @@ namespace Turing.Wasm
     {
         public float x, y, z, w;
     }
+
     
     /// <summary>
     ///  Helper class to convert between C# types and C structs
@@ -25,40 +28,80 @@ namespace Turing.Wasm
 
         // Passthrough converters. These types are already compatible with rust
         [Converter]
-        public static int IntPassthrough(int i)
+        public static int IntPassthrough(int i) // i32
         {
             return i;
         }
 
         [Converter]
-        public static float FloatPassthrough(float f)
+        public static uint UIntPassthrough(uint i) // u32
+        {
+            return i;
+        }
+
+        [Converter]
+        public static long LongPassthrough(long l) // i64
+        {
+            return l;
+        }
+        
+        [Converter]
+        public static ulong ULongPassthrough(ulong l) // u64
+        {
+            return l;
+        }
+
+        [Converter]
+        public static float FloatPassthrough(float f) // f32
         {
             return f;
         }
+        [Converter]
+        public static double DoublePassthrough(double d) // f64
+        {
+            return d;
+        }
+        
+        
 
-        // Back and forth converters
+        // Vec3
         [Converter]
         public static UnityEngine.Vector3 RsVecToUnityVec(Vector3 vec)
         {
             return new UnityEngine.Vector3(vec.x, vec.y, vec.z);
         }
-
         [Converter]
         public static Vector3 UnityVecToRsVec(UnityEngine.Vector3 vec)
         {
             return new Vector3 { x = vec.x, y = vec.y, z = vec.z };
         }
-
+        
+        
+        
+        // Quaternion
         [Converter]
         public static UnityEngine.Quaternion RsQuatToUnityQuat(Quaternion quat)
         {
             return new UnityEngine.Quaternion(quat.x, quat.y, quat.z, quat.w);
         }
-
         [Converter]
         public static Quaternion UnityQuatToRsQuat(UnityEngine.Quaternion quat)
         {
             return new Quaternion { x = quat.x, y = quat.y, z = quat.z, w = quat.w };
+        }
+        
+        
+        
+        // Boolean
+        [Converter]
+        public static RustBool CsBoolToRsBool(bool b)
+        {
+            return new RustBool { value = (byte)(b ? 1 : 0) };
+        }
+        [Converter]
+        public static bool RsBoolToCsBool(RustBool rustBool)
+        {
+            return rustBool.value != 0;
         }
         
     }
---
+++ Turing/Wasm/Wrappers/PrimitiveWrappers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Turing.Wasm.Wrappers
+{
+    public class PrimitiveWrappers
+    {
+        
+    }
+    
+    
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RustBool
+    {
+        public byte value;
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RsString
+    {
+        public IntPtr strPtr;
+        
+        public RsString(string s)
+        {
+            strPtr = Marshal.StringToHGlobalUni(s);
+        }
+
+        public override string ToString()
+        {
+            return Marshal.PtrToStringUni(strPtr) ?? string.Empty;
+        }
+
+        public void Free()
+        {
+            Marshal.FreeHGlobal(strPtr);
+        }
+
+    }
+    
+    
+}
